204037140a1cae8c2f36d10bc7356d1131f796d3
buhaymarella <marella.gutierrez84@gmail.com>
2022-04-04 14:21:56 +0800

    My Educational Background added

--- git-python-buhay.py
+++ git-python-buhay.py
@@ -1,3 +1,7 @@
 print("Marella Buhay")
 print("Hupi, Santa Cruz, Marinduque")
 print("September 17, 2001")
+print("College of Engineering, Information and Industrial Technology")
+print("Bachelor of Science in Information Technology")
+print("Second Year")
+print("Section- B")
